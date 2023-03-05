c833845d089c41a326da9261c5233a890f7e03c6
fahad Mridha <sohelcse16@gmail.com>
2023-03-05 21:08:15 +0600

    added something

--- src/pages/ocean/Ocean.js
+++ src/pages/ocean/Ocean.js
@@ -4,7 +4,6 @@ const Ocean = () => {
   return (
     <div>
       <h3>This is ocean</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quis eaque cum placeat quidem, eos aliquam recusandae possimus, quod tempore quasi repellendus ea! Quod sunt magni saepe est voluptatem, eos, fuga aperiam porro incidunt veritatis dolores autem quas aliquid aliquam, facilis beatae. Expedita, neque nostrum perferendis asperiores in magni dignissimos aspernatur ipsam dolores totam? Tempora, cupiditate velit sapiente sit temporibus deserunt eaque laborum qui fugit est dolor dignissimos atque? Adipisci vel minima repellendus blanditiis magnam at ea nostrum sit, tempora molestias suscipit beatae fuga temporibus eius recusandae corporis nemo quod excepturi cumque iusto! Ab voluptate maxime eum! Officia, nihil debitis.</p>
     </div>
   );
 };
